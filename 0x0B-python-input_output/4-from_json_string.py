bdf10604f957374b34a52923b80a8011bcec2740
selina-nancy <99270932+selina-nancy@users.noreply.github.com>
2022-07-05 19:19:47 +0300

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Module that contains a function that appends to a text file
+"""
+Contains the "from_json_string" function
 """
 
+import json
 
-def append_write(filename="", text=""):
-    """ Function that appends to a text file
-    """
 
-    with open(filename, 'a', encoding="utf-8") as f:
-        return f.write(text)
+def from_json_string(my_str):
+    """returns an object represented by a JSON string"""
+    return json.loads(my_str)
