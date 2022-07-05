b70a705fd27b927d1af65f7dd0463c0c63dc21fe
selina-nancy <99270932+selina-nancy@users.noreply.github.com>
2022-07-05 19:17:40 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the number of lines
-    of a text file
+"""
+Contains the function "wrtie_file"
 """
 
 
-def number_of_lines(filename=""):
-    """
-    function that returns the number of lines of a text file
-    """
-    count = 0
-    with open(filename, 'r') as f:
-        while f.readline() is not '':
-            count += 1
-        return count
+def write_file(filename="", text=""):
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
